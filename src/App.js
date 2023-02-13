8868cde8e94b5e713a6634801fe41da4491f50f0
Miguel Raatz <miguelraatz_@hotmail.com>
2023-02-13 19:03:08 -0300

    correção requisito 18

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/ranking" component={ Ranking } />
       <Route path="/settings" component={ Settings } />
       <Route path="/game" component={ Game } />
       <Route path="/feedback" component={ Feedback } />
---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Ranking extends React.Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Início
+        </button>
+      </div>
+    );
+  }
+}
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Ranking);
